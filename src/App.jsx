176ee0a2942d99501b3abadadb5480544a6f71b4
Kasia <kajzerkatarzyna1@gmail.com>
2023-10-24 15:34:09 +0200

    Added display of parts of plans after selecting a plan.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton.jsx";
-import {PLANS} from "./data.js";
+import {PARTS_OF_PLANS, PLANS} from "./data.js";
 import Card from "./components/UI/Card.jsx";
 import {useState} from "react";
 
@@ -11,6 +11,32 @@ function App() {
     const handleSelect = selectedButton => {
         setSelectedPlan(selectedButton);
     };
+
+    let tabContent = <p>Wybierz plan</p>;
+
+    if (selectedPlan) {
+        tabContent = (
+            <div id="tab-content">
+                <section id="parts_of_plans" className="plans-buttons">
+                    <ul>
+                        <Card className="card-brighter">
+                        {PARTS_OF_PLANS.map((partOfPlan) => (
+                            <TabButton
+                                key={partOfPlan.title}
+                                {...partOfPlan}
+                                isSelected={selectedPlan === `${partOfPlan.title}`}
+                                onSelect={() => handleSelect(`${partOfPlan.title}`)}
+                            >
+                                {partOfPlan.title}
+                            </TabButton>
+                        ))}
+                        </Card>
+                    </ul>
+                </section>
+            </div>
+        );
+    }
+
 
     return (
         <div>
@@ -26,11 +52,14 @@ function App() {
                                     {...plan}
                                     isSelected={selectedPlan === `${plan.title}`}
                                     onSelect={() => handleSelect(`${plan.title}`)}
-                                    >
+                                >
                                     {plan.title}
                                 </TabButton>
                             ))}
                         </ul>
+                    </section>
+                    <section>
+                        {tabContent}
                     </section>
                 </Card>
             </main>
