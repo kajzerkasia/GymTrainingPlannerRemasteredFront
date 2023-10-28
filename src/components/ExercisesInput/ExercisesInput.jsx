b8964e2910638df48343af691898b7e0b31d2e92
Kasia <kajzerkatarzyna1@gmail.com>
2023-10-28 21:47:22 +0200

    Added inputs to ExercisesInput.jsx and added lifted up state "exercisesInput" in App.jsx

--- src/components/ExercisesInput/ExercisesInput.jsx
+++ src/components/ExercisesInput/ExercisesInput.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
 import Section from "../Section.jsx";
 
-const ExercisesInput = () => {
+const ExercisesInput = ({ onChange, exercisesInput }) => {
     return (
         <Section>
+            <div className="single-exercises-input">
+                <p>
+                    <label>Kolejność</label>
+                    <input
+                        type="text"
+                        required
+                        value={exercisesInput.order}
+                        onChange={(event) =>
+                            onChange('order', event.target.value)
+                    }
+                    />
+                </p>
+            </div>
             <div>
                 <p>
-                    <label></label>
-                    <input type="text"/>
+                    <label>Ćwiczenie</label>
+                    <input
+                        type="text"
+                        required
+                        value={exercisesInput.exercise}
+                        onChange={(event) =>
+                            onChange('exercise', event.target.value)
+                        }
+                    />
+                </p>
+            </div>
+            <div>
+                <p>
+                    <label>Ilość serii i powtórzeń</label>
+                    <input
+                        type="text"
+                        required
+                        value={exercisesInput.seriesAndRepetitions}
+                        onChange={(event) =>
+                            onChange('seriesAndRepetitions', event.target.value)
+                        }
+                    />
+                </p>
+            </div>
+            <div>
+                <p>
+                    <label>Tempo</label>
+                    <input
+                        type="text"
+                        required
+                        value={exercisesInput.tempo}
+                        onChange={(event) =>
+                            onChange('tempo', event.target.value)
+                        }
+                    />
+                </p>
+            </div>
+            <div>
+                <p>
+                    <label>Długość przerwy</label>
+                    <input
+                        type="text"
+                        required
+                        value={exercisesInput.rest}
+                        onChange={(event) =>
+                            onChange('rest', event.target.value)
+                        }
+                    />
                 </p>
             </div>
         </Section>
