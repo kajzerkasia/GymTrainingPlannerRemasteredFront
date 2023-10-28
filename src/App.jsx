f99d3fa1c4b9103d37857cfddbd479fafa5bb728
Kasia <kajzerkatarzyna1@gmail.com>
2023-10-28 21:36:37 +0200

    Created new component - ExercisesInput.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import './App.css'
 import Header from "./components/Header/Header.jsx";
 import Plans from "./components/Plans/Plans.jsx";
 import Exercises from "./components/Exercises/Exercises.jsx";
+import {useState} from "react";
 
 function App() {
+    const [exercisesInput, setExercisesInput] = useState({
+        order: '',
+        exercise: '',
+        seriesAndRepetitions: '',
+        tempo: '',
+        rest: '',
+    })
 
     return (
         <div>
