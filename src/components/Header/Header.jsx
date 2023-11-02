b8eaee0a6e5caf57fbe0b74f77d6e203ab1bcf8d
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-02 12:50:46 +0100

    Moved Header CSS styles to classes in index.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-// import './Header.css';
 
 const Header = () => {
   return (
-    <header className="text-center my-12 mx-0">
+    <header className="header">
       {/*<img src="" alt=""/>*/}
-      <h1 className="text-5xl font-header font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink to-cyan pb-4">
-        Gym Training Planner
-      </h1>
-      <p className="text-violet text-sm">
+      <h1 className="header-h1">Gym Training Planner</h1>
+      <p className="header-p">
         Twoja przestrzeń do zarządzania planami treningowymi
       </p>
     </header>
