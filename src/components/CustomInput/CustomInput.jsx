a0eb5cbad2f606196910313822859602e2c13374
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-02 12:28:39 +0100

    Refactored CustomInput.jsx component - added there input to leaner usage of its instance, moved tailwind CSS Section classes from ExercisesInput.jsx to index.css

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-const CustomInput = ({ labelText, children, ...props }) => {
+const CustomInput = ({ labelText, children, value, onChange, ...props }) => {
   return (
     <div>
       <p>
-        <label {...props}>{labelText}</label>
-        {children}
+        <label className="exercises-label">{labelText}</label>
+        <input
+          type="text"
+          required
+          className="exercises-input"
+          onChange={onChange}
+          value={value}
+          {...props}
+        />
       </p>
     </div>
   );
--- src/components/ExercisesInput/ExercisesInput.jsx
+++ src/components/ExercisesInput/ExercisesInput.jsx
@@ -4,57 +4,34 @@ import CustomInput from "../CustomInput/CustomInput.jsx";
 
 const ExercisesInput = ({ onChange, exercisesInput }) => {
   return (
-    <Section className="p-4 w-2/5 my-8 mx-0 rounded bg-violet">
-      <CustomInput labelText="Kolejność" className="exercises-label">
-        <input
-          className="exercises-input"
-          type="text"
-          required
-          value={exercisesInput.order}
-          onChange={(event) => onChange("order", event.target.value)}
-        />
-      </CustomInput>
-      <CustomInput labelText="Ćwiczenie" className="exercises-label">
-        <input
-          className="exercises-input"
-          type="text"
-          required
-          value={exercisesInput.exercise}
-          onChange={(event) => onChange("exercise", event.target.value)}
-        />
-      </CustomInput>
+    <Section className="section">
+      <CustomInput
+        labelText="Kolejność"
+        value={exercisesInput.order}
+        onChange={(event) => onChange("order", event.target.value)}
+      />
+      <CustomInput
+        labelText="Ćwiczenie"
+        value={exercisesInput.exercise}
+        onChange={(event) => onChange("exercise", event.target.value)}
+      />
       <CustomInput
         labelText="Ilość serii i powtórzeń"
-        className="exercises-label"
-      >
-        <input
-          className="exercises-input"
-          type="text"
-          required
-          value={exercisesInput.seriesAndRepetitions}
-          onChange={(event) =>
-            onChange("seriesAndRepetitions", event.target.value)
-          }
-        />
-      </CustomInput>
-      <CustomInput labelText="Tempo" className="exercises-label">
-        <input
-          className="exercises-input"
-          type="text"
-          required
-          value={exercisesInput.tempo}
-          onChange={(event) => onChange("tempo", event.target.value)}
-        />
-      </CustomInput>
-      <CustomInput labelText="Długość przerwy" className="exercises-label">
-        <input
-          className="exercises-input"
-          type="text"
-          required
-          value={exercisesInput.rest}
-          onChange={(event) => onChange("rest", event.target.value)}
-        />
-      </CustomInput>
+        value={exercisesInput.seriesAndRepetitions}
+        onChange={(event) =>
+          onChange("seriesAndRepetitions", event.target.value)
+        }
+      />
+      <CustomInput
+        labelText="Tempo"
+        value={exercisesInput.tempo}
+        onChange={(event) => onChange("tempo", event.target.value)}
+      />
+      <CustomInput
+        labelText="Długość przerwy"
+        value={exercisesInput.rest}
+        onChange={(event) => onChange("rest", event.target.value)}
+      />
     </Section>
   );
 };
