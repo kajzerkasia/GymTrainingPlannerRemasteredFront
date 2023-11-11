20f9a9eac672bda76a972f741a17adb367fb287b
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-12 00:48:38 +0100

    Removed unnecessary props

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-const CustomInput = ({ labelText, value, onChange }) => {
-  return (
-    <div>
-      <p>
-        <label className="exercises-label">{labelText}</label>
-        <input
-          type="text"
-          required
-          className="exercises-input"
-          onChange={onChange}
-          value={value}
-        />
-      </p>
-    </div>
-  );
+const CustomInput = ({labelText, ...props}) => {
+    return (
+        <div>
+            <p>
+                <label className="exercises-label">{labelText}</label>
+                <input
+                    {...props}
+                    type="text"
+                    required
+                    className="exercises-input"
+                />
+            </p>
+        </div>
+    );
 };
 
 export default CustomInput;
