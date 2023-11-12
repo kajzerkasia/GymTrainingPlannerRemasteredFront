3582d5fb143fef8292c956449f4bdf8e334a11c7
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-12 16:54:48 +0100

    Moved all Exercises logic and components to one common.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Plans from "./components/Plans/Plans.jsx";
-import ExercisesData from "./components/ExercisesData/ExercisesData.jsx";
-import {useState} from "react";
-import ExercisesInput from "./components/ExercisesInput/ExercisesInput.jsx";
-import ExercisesColumnsNames from "./components/ExercisesColumnsNames/ExercisesColumnsNames.jsx";
-import {INITIAL_EXERCISES_INPUT} from "./constants/data.js";
+import Exercises from "./components/Exercises/Exercises.jsx";
 
 function App() {
-    const [exercisesInput, setExercisesInput] = useState(INITIAL_EXERCISES_INPUT);
-    const [exercisesArray, setExercisesArray] = useState([]);
 
-    function handleChange(inputIdentifier, newValue) {
-        setExercisesInput((prevExercisesInput) => {
-            return {
-                ...prevExercisesInput,
-                [inputIdentifier]: newValue,
-            };
-        });
-    }
-
-    const handleSaveClick = (newExercise) => {
-        const {order, exercise, seriesAndRepetitions, tempo, rest} = newExercise;
-        const updatedExercisesArray = [...exercisesArray];
-        setExercisesArray(updatedExercisesArray);
-        setExercisesArray((prevExercisesArray) => [
-            ...prevExercisesArray,
-            {
-                order,
-                exercise,
-                seriesAndRepetitions,
-                tempo,
-                rest,
-            }
-        ]);
-        return exercisesArray;
-    }
-
-    const resetExercisesInput = () => {
-        setExercisesInput(INITIAL_EXERCISES_INPUT);
-    };
 
     return (
         <div>
@@ -47,14 +12,7 @@ function App() {
             <main>
                 <Plans/>
             </main>
-            <ExercisesInput
-                exercisesInput={exercisesInput}
-                onChange={handleChange}
-                onSaveClick={handleSaveClick}
-                onReset={resetExercisesInput}
-            />
-            <ExercisesColumnsNames/>
-            <ExercisesData exercisesArray={exercisesArray}/>
+            <Exercises/>
         </div>
     );
 }
---
+++ src/components/Exercises/Exercises.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import ExercisesInput from "../ExercisesInput/ExercisesInput.jsx";
+import ExercisesColumnsNames from "../ExercisesColumnsNames/ExercisesColumnsNames.jsx";
+import ExercisesData from "../ExercisesData/ExercisesData.jsx";
+import {INITIAL_EXERCISES_INPUT} from "../../constants/data.js";
+
+const Exercises = () => {
+
+    const [exercisesInput, setExercisesInput] = useState(INITIAL_EXERCISES_INPUT);
+    const [exercisesArray, setExercisesArray] = useState([]);
+
+    function handleChange(inputIdentifier, newValue) {
+        setExercisesInput((prevExercisesInput) => {
+            return {
+                ...prevExercisesInput,
+                [inputIdentifier]: newValue,
+            };
+        });
+    }
+
+    const handleSaveClick = (newExercise) => {
+        const {order, exercise, seriesAndRepetitions, tempo, rest} = newExercise;
+        const updatedExercisesArray = [...exercisesArray];
+        setExercisesArray(updatedExercisesArray);
+        setExercisesArray((prevExercisesArray) => [
+            ...prevExercisesArray,
+            {
+                order,
+                exercise,
+                seriesAndRepetitions,
+                tempo,
+                rest,
+            }
+        ]);
+        return exercisesArray;
+    }
+
+    const resetExercisesInput = () => {
+        setExercisesInput(INITIAL_EXERCISES_INPUT);
+    };
+
+    return (
+        <div>
+            <ExercisesInput
+                exercisesInput={exercisesInput}
+                onChange={handleChange}
+                onSaveClick={handleSaveClick}
+                onReset={resetExercisesInput}
+            />
+            {exercisesArray.length > 0 && <ExercisesColumnsNames/>}
+            <ExercisesData exercisesArray={exercisesArray}/>
+        </div>
+    );
+};
+
+export default Exercises;
