494e0b214f24c11b8bb41c4218a499706e50451b
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-12 18:57:39 +0100

    Created and added constants with keys to Exercises.jsx, PartsOfPlan.jsx and Plans.jsx

--- src/components/PartsOfPlan/PartsOfPlan.jsx
+++ src/components/PartsOfPlan/PartsOfPlan.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {useInputLogic} from "../../hooks/useInputLogic.js";
 import PartsOfPlanData from "../PartsOfPlanData/PartsOfPlanData.jsx";
 import PartsOfPlanInput from "../PartsOfPlanInput/PartsOfPlanInput.jsx";
+import {PARTS_OF_PLAN_KEYS} from "../../constants/data.js";
 
 const PartsOfPlan = () => {
-    const keys = ["title"];
     const {
         input,
         itemsArray,
         handleChange,
         handleSaveClick,
         handleReset,
-    } = useInputLogic(keys);
+    } = useInputLogic(PARTS_OF_PLAN_KEYS);
 
     return (
         <div>
