6b7ebe8bb4cc1dc9f7c9e55f21ad8118ee6af6f0
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-20 13:24:03 +0100

    Replaced useInputLogic.js with input-context.jsx that use context and reducer to manage state. Used input-context.jsx in all Plans, PartsOfPlans and Exercise components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Plans from "./components/Plans/Plans.jsx";
+import PartsOfPlan from "./components/PartsOfPlan/PartsOfPlan.jsx";
+import React from "react";
+import Exercises from "./components/Exercises/Exercises.jsx";
+import {InputContextProvider} from "./store/input-context.jsx";
 
 function App() {
 
 
     return (
         <div>
+            <InputContextProvider>
             <Header/>
             <Plans/>
+            <PartsOfPlan/>
+            <Exercises/>
+            </InputContextProvider>
         </div>
     );
 }
--- src/constants/data.js
+++
@@ -1,5 +0,0 @@
-export const EXERCISES_KEYS = ["order", "exercise", "seriesAndRepetitions", "tempo", "rest"];
-
-export const PLANS_KEYS = ["title"];
-
-export const PARTS_OF_PLAN_KEYS = ["title"];
